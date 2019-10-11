e5ea220933e9f3b4bdf8184d45d2729e12ef95a5
jackymxp <mengxiangpeng@mengxiangpengdeMacBook-Pro.local>
2019-10-11 17:25:38 +0800

    add loopqueue

---
+++ 03-queue/loopqueue.hpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class LoopQueue
+{
+private:
+    T* data;
+    int head, tail;
+    int count;
+    int capacity;
+    void resize(int newCap) 
+    {
+        T* newData = new T[newCap];
+        for(int i = 0; i < count; i++)
+        {
+            newData[i] = data[(i+head)%capacity];
+        }
+        delete data;
+        data = newData;
+        head = 0;
+        tail = count;
+        capacity = newCap;
+    }
+public:
+    //入队tail = (tail + 1) % 数组长度
+    //出队head = (head + 1) % 数组长度
+    //head == tail 对列为空
+    //(tail + 1) % 数组长度 == head 队列满
+    LoopQueue(int cap)
+    {
+        capacity = cap + 1; //要有一个元素空着，标志数组为满
+        data = new T[capacity];
+        head = 0;
+        tail = 0;//[head tail) 使用的是这个范围内的数据，作为队列
+        count = 0;
+    }
+    
+    LoopQueue()
+    {
+        capacity = 11;
+        data = new T[capacity];
+        head = 0;
+        tail = 0;
+        count = 0;
+    }
+    ~LoopQueue()
+    {
+        delete[] data;
+    }
+    int getCapacity(){return capacity - 1;}
+    int size(){return count;}
+    bool isEmpty(){return head == tail;}
+    void push(T e)
+    {
+        if((tail+1)%capacity == head)
+            resize(getCapacity() * 2);
+        data[tail] = e;
+        tail = (tail + 1) % capacity;
+        count++;
+    }
+    T pop()
+    {
+        assert(isEmpty() == false);
+        T ret = data[head];
+        head = (head + 1) % capacity;
+        count--;
+        if(count == getCapacity() / 4 && getCapacity() / 2 != 0) 
+            resize(getCapacity() / 2);
+        return ret;
+    }
+    T front()
+    {
+        assert(isEmpty() == false);
+        return data[head];
+    }
+    template<typename T1>
+    friend ostream& operator<<(ostream& os, LoopQueue<T1>& s);
+};
+
+template<typename T>
+ostream& operator<<(ostream& os, LoopQueue<T>& s)
+{
+    os << "loopqueue size = " << s.size() << "    loopqueue capacity = " << s.getCapacity() << endl;
+    os << "[";
+    for(int i = s.head; i != s.tail; i = (i+1)%s.capacity)
+    {
+        os << s.data[i];
+        if((i+1)%s.capacity != s.tail)
+            os << ", ";
+    }
+    os << "] top" << endl;
+    return os;
+}
+
+
--- 03-queue/main.cpp
+++ 03-queue/main.cpp
@@ -1,5 +1,5 @@
 #include "queue.hpp"
-
+#include "loopqueue.hpp"
 class Student
 {
 public:
@@ -15,9 +15,9 @@ ostream& operator<<(ostream& os, Student& s)
     os << "age = " << s.age << "  score = " << s.score << endl;
     return os;
 }
-int main(void)
+
+void testQueue()
 {
-    cout << "test queue " << endl;
     Queue<int> q1;
     for(int i = 0; i < 20; i++)
         q1.push(i);
@@ -40,4 +40,61 @@ int main(void)
         s2.pop();
         cout << ss << endl;
     }
+}
+void testLoopQueue()
+{
+    LoopQueue<int> q1;
+    for(int i = 0; i < 20; i++)
+        q1.push(i);
+
+    cout << q1 << endl;
+    cout << q1.front() << endl;
+    cout << q1.pop() << endl;
+    cout << q1 << endl;
+
+    LoopQueue<Student> s2;
+    for(int i = 0; i < 50; i++)
+    {   
+        Student t(i, i*10);
+        s2.push(t);
+    }
+    
+    while(!s2.isEmpty())
+    {
+        Student ss = s2.front();
+        s2.pop();
+        cout << ss << endl;
+    }
+}
+void compare(int oc)
+{
+    Queue<int> q1;
+    srand(time(NULL));
+    clock_t start1 = clock();
+    for(int i = 0; i < oc; i++)
+        q1.push(random());
+    for(int i = 0; i < oc; i++)
+        q1.pop();
+    clock_t end1 = clock();
+    cout << "queue take " << (double)(end1 - start1) / CLOCKS_PER_SEC << " s  "  << endl;
+    
+    LoopQueue<int> q2;
+    clock_t start2 = clock();
+    for(int i = 0; i < oc; i++)
+        q2.push(random());
+    for(int i = 0; i < oc; i++)
+        q2.pop();
+    clock_t end2 = clock();
+    cout << "loopqueue take " << (double)(end2 - start2) / CLOCKS_PER_SEC << " s  "  << endl;
+}
+int main(void)
+{
+    cout << "test queue " << endl;
+    testQueue();
+    
+    cout << "test loopqueue  " << endl; 
+    testLoopQueue();
+    
+    compare(1000000);
+    return 0; 
 }

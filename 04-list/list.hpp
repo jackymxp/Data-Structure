241f7ba9fd45c9ccf52520b04cb7cfe127169e02
jackymxp <mengxiangpeng@mengxiangpengdeMacBook-Pro.local>
2019-10-14 20:59:08 +0800

    mend

---
+++ 04-list/list.hpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class List
+{
+private:
+    class Node
+    {
+    public:
+        T data;
+        Node* next;
+        Node(T d):data(d), next(NULL) {}
+        Node(T d, Node* n):data(d), next(n){}
+        Node():next(NULL) {}
+    };
+    Node* head;
+    int count;
+public:
+    List():head(NULL), count(0){}
+    int size() {return count;}
+    bool isEmpty() {return count == 0;}
+    void addFirst(T e)
+    {
+        Node* node = new Node(e);
+        node->next = head;
+        head = node;
+        count++;
+    }
+    
+    void add(int index, T e)
+    {
+        assert(index >= 0 && index <= count);
+        if(index == 0)  
+            addFirst(e);
+        else
+        {
+            Node* prev = head;
+            for(int i = 0; i < index - 1; i++)
+                prev = prev->next;
+            Node* node = new Node(e);
+            node->next = prev->next;
+            prev->next = node;
+            count++;
+        }
+    }
+    void addLast(T e)
+    {
+        add(e, count);
+    }
+
+    template<typename T1> 
+    friend ostream& operator<<(ostream& os, List<T1>& p);
+};
+
+#if 1
+template<typename T>
+ostream& operator<<(ostream& os, List<T>& p)
+{
+    os << "size = " << p.count << endl;
+    auto h = p.head;
+    while(h)
+    {
+        if(h->next)
+            os << h->data << "-->" ;
+        else
+            os << h->data;
+        h = h->next;
+    }
+    return os;
+} 
+#endif
---
+++ 04-list/main.cpp
@@ -0,0 +1,15 @@
+#include "list.hpp"
+
+int main(void)
+{
+    cout << "test list" << endl;
+    
+    List<int> l;
+    for(int i = 0; i < 10; i++) 
+        l.addFirst(i);
+
+    cout << l << endl;
+
+
+    return 0;
+}

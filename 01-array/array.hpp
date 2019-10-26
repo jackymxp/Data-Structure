bb852b7afdb36050167190d5bd423ca400067bd7
jackymxp <mengxiangpeng@mengxiangpengdeMacBook-Pro.local>
2019-10-26 18:11:13 +0800

    array + constructure function

--- 01-array/array.hpp
+++ 01-array/array.hpp
@@ -34,6 +34,14 @@ public:
         capacity = 10;
         data = new T[capacity];
     }
+    Array(int cap, T e)
+    {
+        count = cap;
+        capacity = cap;
+        data = new T[capacity];
+        for(int i = 0; i < cap; i++)
+            data[i] = e;
+    }
     int getCapacity() { return capacity; }
     int size(){return count;}
     bool empty(){return count == 0;}
